d8abaca13728aa52c9b3a5d94de77e5c61eca601
Zack Strickland <zas0923@gmail.com>
2015-07-07 15:52:33 -0400

    Add Devise test helpers and sign_in method to controller tests.

--- db/schema.rb
+++ db/schema.rb
@@ -92,5 +92,5 @@ ActiveRecord::Schema.define(version: 20150707174051) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
+
 end
--- test/controllers/dashboards_controller_test.rb
+++ test/controllers/dashboards_controller_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class DashboardsControllerTest < ActionController::TestCase
-  
+  setup do
+    sign_in users(:one)
+  end
 
   test "should get index" do
     get :index
