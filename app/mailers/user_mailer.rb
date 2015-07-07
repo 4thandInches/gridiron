39932f6ad7178786fe2634cbe5e49d4d91dc83a7
Zack Strickland <zas0923@gmail.com>
2015-07-07 09:08:41 -0400

    Work on mailer for Devise sign-up

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,8 @@
 require 'mailgun'
 class UserMailer < ApplicationMailer
 
-  def send_test_email
-    mail(to: "dadkisso@live.com", subject: "Test Email From Gridiron")
+  def send_welcome_email
+    mail(to: "ZAS0923@gmail.com", subject: "Test Email From Gridiron")
 
   end
 end
