8b2d8093c04473d080b9df50c95953b2e40cb16c
Scott Williams <williamsjs1224@gmail.com>
2015-07-10 13:08:04 -0400

    players can be dragged and dropped on depth chart

--- app/models/position.rb
+++ app/models/position.rb
@@ -3,7 +3,13 @@ class Position < ActiveRecord::Base
   has_many :players, through: :roles
 
   def get_team_players(team)
-    players.where(team_id: team.id)
+    team_roles = self.roles.order(:depth_chart_position).joins(:player).where("players.team_id = #{team.id}")
+    team_roles.map &:player
+
+  end
+
+  def sort_by_depth(positions)
+
   end
 
 end
