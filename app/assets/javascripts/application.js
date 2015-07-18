c571150e64383a59c230fbda8b99ab7f2bc0adca
Scott Williams <williamsjs1224@gmail.com>
2015-07-18 14:03:48 -0400

    add modal for pdf display

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -343,9 +343,5 @@ $(document).ready(function() {
 
 //Plays Modal
 $(function() {
-  //   $('.pdf-lightbox').on('click', function() {
-  //     var address = $(this).attr('href');
-  //     window.open(address, 'google', 'height=500,width=800');
-  // })
   $('.pdf-lightbox').magnificPopup({type:'iframe'})
 });
