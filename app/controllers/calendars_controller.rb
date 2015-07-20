a669c9f48decedd00e2f29f082290bd7102b6063
Scott Williams <williamsjs1224@gmail.com>
2015-07-20 10:02:57 -0400

    add multi-tenancy to team

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,4 +1,14 @@
 class CalendarsController < ApplicationController
+  before_action :set_team
+
   def index
+    @events = Event.where(team: @team)
   end
+
+  private
+
+    def set_team
+      @team = Team.find(current_user.team_id)
+    end
+
 end
